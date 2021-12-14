3937595a658390bbf0b9d477bf2d6be66088bfcd
Julio Placeres <julio.placeres@hillderm.local>
2021-12-14 13:51:58 -0500

    added gitignore file on client and server for node modules to not upload to github, also removed a few innecesary dependencies from json files

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
 const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-// Add typeDefs and resolvers if needed later on
-// const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const server = new ApolloServer({});
-
-// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -29,6 +23,5 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });

961a7a03163e71101f48f77e97ba7343c94198e5
dreamheart <leondreamheart@outlook.com>
2022-03-12 16:27:00 -0500

    Added Navbar folder and added phone and email to topnavbar

---
+++ client/src/components/Link-Icons/Email.js
@@ -0,0 +1,18 @@
+import { HiOutlineMail } from "react-icons/hi";
+
+const Email = () => {
+  return (
+    <div class="container">
+      <div class="">
+        <small>
+          <HiOutlineMail />
+          <span class="">
+            <a href="tel:+1 (309) 533-9965"> +1 (309) 533-9965 </a>
+          </span>
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default Email;
---
+++ client/src/components/Link-Icons/Phone.js
@@ -0,0 +1,18 @@
+import { AiFillPhone } from "react-icons/ai";
+
+const Phone = () => {
+  return (
+    <div class="container">
+      <div class="">
+        <small>
+          <AiFillPhone />
+          <span class="">
+            <a href="tel:+1 (309) 533-9965"> +1 (309) 533-9965 </a>
+          </span>
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default Phone;

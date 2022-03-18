234d47f4b5387e45426c5de087843c445bffca3a
dreamheart <leondreamheart@outlook.com>
2022-03-17 22:44:24 -0400

    remove text from phone and email as it takes too much space. aligned icons to mobile version (xs)

--- client/src/components/Link-Icons/Email.js
+++ client/src/components/Link-Icons/Email.js
@@ -3,12 +3,9 @@ import { MdEmail } from "react-icons/md";
 const Email = () => {
   return (
     <>
-      <small>
+      <a href="mailto:info@yourcompany.example.com">
         <MdEmail />
-          <a href="mailto:info@yourcompany.example.com">
-            toptiercabinetsrefinishing@gmail.com
-          </a>
-      </small>
+      </a>
     </>
   );
 };

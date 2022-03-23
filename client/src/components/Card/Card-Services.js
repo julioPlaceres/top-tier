55db64bca765d1bf893171011846a4c64259c561
Julio Placeres <julio.placeres@hillderm.local>
2022-03-23 16:53:58 -0400

    Added props to the card component

--- client/src/components/Card/Card-Services.js
+++ client/src/components/Card/Card-Services.js
@@ -1,17 +1,14 @@
 import "./Card-Services.css";
 import { Card, Button } from "react-bootstrap";
-import Cabinets from "../images/Cabinets.jpg";
 
-const CardServices = () => {
+const CardServices = (props) => {
   return (
 <Card border="light" style={{ width: "20rem"}} className="mx-auto">
-    <Card.Img variant="top" src={Cabinets} alt="card image" style={{maxWidth: "200px", backgroundColor: "brown"}} className="mx-auto"/>
+    <Card.Img variant="top" src={props.src} alt="card image" style={{maxWidth: "200px", backgroundColor: "brown"}} className="mx-auto"/>
       <Card.Body style={{backgroundColor: "brown"}}>
-        <Card.Title>Cabinet Painting</Card.Title>
-        <Card.Text> <small>TopTier provides you with excellency when it comes to cabinet painting/repairing. With high quality paint and
-          equipment, your cabinets will look new, see our gallery for photos of happy satisfy customers.
-          </small></Card.Text>
-        <Button variant="primary">See more</Button>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text> <small>{props.text}</small></Card.Text>
+        <Button variant="primary">{props.btnText}</Button>
       </Card.Body>
 </Card>
   );
--- client/src/components/Services-Section/Services-Section.js
+++ client/src/components/Services-Section/Services-Section.js
@@ -1,11 +1,20 @@
 import "./Services-Section.css";
 import { Container } from "react-bootstrap";
 import CardServices from "../Card/Card-Services";
+import Cabinets from "../images/Cabinets.jpg";
 
 const ServicesSection = () => {
   return (
     <Container className="services-background">
-      <CardServices />
+      <CardServices title="Cabinet Painting"
+      text="TopTier provides you with excellency when it comes to cabinet painting/repairing. With high quality paint and
+      equipment, your cabinets will look new, see our gallery for photos of happy satisfy customers."
+      btnText="See more"
+      src={Cabinets}/>
+
+<CardServices title="Cabinet Repairing"
+      text="Top Tier repairing services."
+      btnText="See gallery"/>
     </Container>
   );
 };

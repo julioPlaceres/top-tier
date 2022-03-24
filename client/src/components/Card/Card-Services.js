c2efef5120e394bd064c9180921a61fd276912ff
Julio Placeres <julio.placeres@hillderm.local>
2022-03-24 08:58:28 -0400

    Added more images for resources, renamed images for easy identification

--- client/src/components/Card/Card-Services.js
+++ client/src/components/Card/Card-Services.js
@@ -3,19 +3,20 @@ import { Card, Button } from "react-bootstrap";
 
 const CardServices = (props) => {
   return (
-<Card border="light" 
-style={{ width: "20rem", borderRadius: "45px"}} 
-className="mx-auto card-wood"> 
+<Card border="light" style={{ width: "20rem", borderRadius: "50px"}} className="mx-auto card-wood"> 
+    
     <Card.Img variant="top" 
     src={props.src} 
     alt="card image" 
-    style={{maxWidth: "200px"}} 
-    className="mx-auto rounded float-start"/>
+    style={{maxWidth: "200px", borderRadius: "100px"}} 
+    className="mx-auto"/>
+      
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text> <small>{props.text}</small></Card.Text>
+        <Card.Title style={{color: "black"}}>{props.title}</Card.Title>
+        <Card.Text style={{color: "black"}}> <small>{props.text}</small></Card.Text>
         <Button variant="primary">{props.btnText}</Button>
       </Card.Body>
+      
 </Card>
   );
 };

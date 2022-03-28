8862c84ec39b9c5702dc3d286ecb82aaf807fed3
Julio Placeres <julio.placeres@hillderm.local>
2022-03-28 16:26:55 -0400

    Added new images and icons, Replaced the images on the service section by icons and gave it a transparent background

--- client/src/components/Card/Card-Services.js
+++ client/src/components/Card/Card-Services.js
@@ -3,18 +3,18 @@ import { Card, Button } from "react-bootstrap";
 
 const CardServices = (props) => {
   return (
-<Card border="light" style={{ width: "20rem", borderRadius: "50px"}} className="mx-auto card-wood"> 
+<Card border="light" style={{ width: "20rem", borderRadius: "50px", maxHeight: "600px"}} className="mx-auto card-wood"> 
     
     <Card.Img variant="top" 
     src={props.src} 
     alt="card image" 
-    style={{maxWidth: "200px", borderRadius: "100px"}} 
+    style={{maxWidth: "100px"}} 
     className="mx-auto"/>
       
       <Card.Body>
         <Card.Title style={{color: "black"}}>{props.title}</Card.Title>
         <Card.Text style={{color: "black"}}> <small>{props.text}</small></Card.Text>
-        <Button variant="primary">{props.btnText}</Button>
+        <Button variant="info">{props.btnText}</Button>
       </Card.Body>
       
 </Card>
